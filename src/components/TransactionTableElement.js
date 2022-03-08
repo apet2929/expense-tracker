3b6f5b068b4e1be42338b32eadfdcb7223469364
Andrew Petersen <realandrewpetersen@gmail.com>
2022-03-08 10:17:24 -0600

    About to refactor everything to the new layout

---
+++ src/components/TransactionTableElement.js
@@ -0,0 +1,10 @@
+import { useState } from "react/cjs/react.production.min";
+
+
+function TransactionTableElement(props) {
+    let id = useState(props.id);
+    let date = useState(props.date);
+    let amount = useState(props.amount);
+    let description = useState(props.description);
+    let category = useState(props.category);
+}
